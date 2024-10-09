b346abfadc8c801518643c299a01c2140f699ce4
aminulhoquenusu <thatsnajmul@yahoo.com>
2024-10-09 18:56:35 +0600

    Add

---
+++ android_project/My App/app/src/main/java/com/thatsnajmul/myapp/WebViewActivity.java
@@ -0,0 +1,30 @@
+package com.thatsnajmul.myapp;
+
+
+import android.os.Bundle;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class WebViewActivity extends AppCompatActivity {
+    private WebView webView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_webview);
+
+        webView = findViewById(R.id.webView);
+        webView.setWebViewClient(new WebViewClient());
+        webView.loadUrl("https://www.yourwebsite.com"); // Replace with your URL
+    }
+
+    @Override
+    public void onBackPressed() {
+        if (webView.canGoBack()) {
+            webView.goBack();
+        } else {
+            super.onBackPressed();
+        }
+    }
+}

89f5a8d9864207aabd3bc29d757c46da9b4a16db
Md Najmul Islam <thatsnajmul@yahoo.com>
2024-10-10 07:44:33 +0600

    Add

--- android_project/My App/app/src/main/java/com/thatsnajmul/myapp/MainActivity.java
+++ android_project/My App/app/src/main/java/com/thatsnajmul/myapp/MainActivity.java
@@ -20,7 +20,7 @@ public class MainActivity extends AppCompatActivity {
         // drawer layout instance to toggle the menu icon to open
         // drawer and back button to close drawer
         drawerLayout = findViewById(R.id.my_drawer_layout);
-        actionBarDrawerToggle = new ActionBarDrawerToggle(this, drawerLayout, R.string.nav_open, R.string.nav_close);
+        actionBarDrawerToggle = new ActionBarDrawerToggle(this, drawerLayout, R.string.open_nav, R.string.close_nav);
 
         // pass the Open and Close toggle for the drawer layout listener
         // to toggle the button

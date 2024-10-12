8953214f2c60e79423d7a8d01f7ab9a589a6ac3f
aminulhoquenusu <thatsnajmul@yahoo.com>
2024-10-12 16:18:05 +0600

    Add

---
+++ android_project/NavDrawer/app/src/main/java/com/thatsnajmul/navdrawer/MainActivity.java
@@ -0,0 +1,137 @@
+package com.thatsnajmul.navdrawer;
+
+import android.os.Bundle;
+import android.util.Log;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ListView;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBarDrawerToggle;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.drawerlayout.widget.DrawerLayout;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+
+public class MainActivity extends AppCompatActivity {
+
+    private String[] mNavigationDrawerItemTitles;
+    private DrawerLayout mDrawerLayout;
+    private ListView mDrawerList;
+    private Toolbar toolbar;
+    private CharSequence mDrawerTitle;
+    private CharSequence mTitle;
+    private ActionBarDrawerToggle mDrawerToggle;
+
+
+    private void setupToolbar() {
+        Toolbar toolbar = findViewById(R.id.toolbar); // Link to the toolbar in your layout
+        setSupportActionBar(toolbar); // Set the custom toolbar as the ActionBar
+        setSupportActionBar(toolbar);  // Use only if no default action bar
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        mTitle = mDrawerTitle = getTitle();
+        mNavigationDrawerItemTitles = getResources().getStringArray(R.array.navigation_drawer_items_array);
+        mDrawerLayout = findViewById(R.id.drawer_layout);
+        mDrawerList = findViewById(R.id.left_drawer);
+
+        setupToolbar();
+
+        DataModel[] drawerItem = new DataModel[3];
+        drawerItem[0] = new DataModel(R.drawable.connect, "Connect");
+        drawerItem[1] = new DataModel(R.drawable.fixtures, "Fixtures");
+        drawerItem[2] = new DataModel(R.drawable.table, "Table");
+
+        // Enable the action bar's home button and set it to act as a drawer toggle
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setHomeButtonEnabled(true);
+
+        DrawerItemCustomAdapter adapter = new DrawerItemCustomAdapter(this, R.layout.list_view_item_row, drawerItem);
+        mDrawerList.setAdapter(adapter);
+        mDrawerList.setOnItemClickListener(new DrawerItemClickListener());
+
+        // Setup the ActionBarDrawerToggle
+        setupDrawerToggle();
+    }
+
+    private class DrawerItemClickListener implements ListView.OnItemClickListener {
+
+        @Override
+        public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+            selectItem(position);
+        }
+
+    }
+
+    private void selectItem(int position) {
+        Fragment fragment = null;
+
+        switch (position) {
+            case 0:
+                fragment = new ConnectFragment();
+                break;
+            case 1:
+                fragment = new FixturesFragment();
+                break;
+            case 2:
+                fragment = new TableFragment();
+                break;
+            default:
+                break;
+        }
+
+        if (fragment != null) {
+            FragmentManager fragmentManager = getSupportFragmentManager();
+            fragmentManager.beginTransaction().replace(R.id.content_frame, fragment).commit();
+
+            mDrawerList.setItemChecked(position, true);
+            mDrawerList.setSelection(position);
+            setTitle(mNavigationDrawerItemTitles[position]);
+            mDrawerLayout.closeDrawer(mDrawerList);
+
+        } else {
+            Log.e("MainActivity", "Error in creating fragment");
+        }
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        // Handle the drawer toggle clicks
+        if (mDrawerToggle.onOptionsItemSelected(item)) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public void setTitle(CharSequence title) {
+        mTitle = title;
+        getSupportActionBar().setTitle(mTitle);
+    }
+
+    @Override
+    protected void onPostCreate(Bundle savedInstanceState) {
+        super.onPostCreate(savedInstanceState);
+        // Sync the state of the drawer toggle after onRestoreInstanceState has occurred.
+        mDrawerToggle.syncState();
+    }
+
+
+    void setupDrawerToggle() {
+        mDrawerToggle = new ActionBarDrawerToggle(
+                this, mDrawerLayout, toolbar, R.string.app_name, R.string.app_name);
+
+        // This is necessary to change the icon of the Drawer Toggle upon state change.
+        mDrawerToggle.syncState();
+        // Add the DrawerToggle as the DrawerListener
+        mDrawerLayout.addDrawerListener(mDrawerToggle);
+    }
+}
---
+++ android_project/NavDrawer/app/src/main/java/com/thatsnajmul/navdrawer/TableFragment.java
@@ -0,0 +1,22 @@
+package com.thatsnajmul.navdrawer;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.fragment.app.Fragment;
+
+public class TableFragment extends Fragment {
+
+    public TableFragment() {
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View rootView = inflater.inflate(R.layout.fragment_table, container, false);
+
+        return rootView;
+    }
+}

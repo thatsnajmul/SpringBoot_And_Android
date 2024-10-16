5cf1260b1a18116d40ef43e3c3e3d0b44c081219
thatsnajmul <thatsnajmul@yahoo.com>
2024-10-16 20:18:34 +0600

    Add

---
+++ render/render-app-api/src/main/java/com/thatsnajmul/render_app_api/RenderAppApiApplication.java
@@ -0,0 +1,13 @@
+package com.thatsnajmul.render_app_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RenderAppApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RenderAppApiApplication.class, args);
+	}
+
+}
---
+++ render/render-app-api/src/test/java/com/thatsnajmul/render_app_api/RenderAppApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.thatsnajmul.render_app_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RenderAppApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

b7d23235e2bef2c950e0256308f6423ad18e827a
thatsnajmul <thatsnajmul@yahoo.com>
2024-10-24 21:03:23 +0600

    Add

--- Flutter/test/widget_test.dart
+++ Flutter/test/widget_test.dart
@@ -7,8 +7,8 @@
 
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
+import 'package:fluttertest/main.dart';
 
-import 'package:job_search/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
